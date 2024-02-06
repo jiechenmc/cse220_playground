3443b1736ec8511d70b453694fd74a163abe96fb
Jie Chen <jiechenmc@gmail.com>
2024-02-06 22:20:48 +0000

    clean

--- debug1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-    int i = 0; 
-    char *string = "Hello, World!";
-    printf("%s\n", string);
-    return EXIT_SUCCESS;
-}
--- debug3.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-#define debug(msg) printf("DEBUG: %s", msg)
-
-int main(int argc, char *argv[]) {
-	debug("Hello, World!\n");
-	return EXIT_SUCCESS;
-}
